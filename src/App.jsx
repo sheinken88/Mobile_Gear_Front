e0d6931f142ec0c4d4b7cf2425f2fa3913009c69
PC <heinken.sebastian@gmail.com>
2023-05-13 19:53:51 -0300

    React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from "react";
-
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { ProductDetail } from "./components/ProductDetail";
+import { Cart } from "./components/Cart";
+import { Checkout } from "./components/Checkout";
+import { OrderHistory } from "./components/OrderHistory";
+import { Admin } from "./components/Admin";
+import { Footer } from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const isAuthenticated = false;
+  const isAdmin = false;
 
   return (
-    <>
-      <h1>div para gonza</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Home />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        {isAuthenticated && (
+          <>
+            <Route path="/cart" element={<Cart />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="/order-history" element={<OrderHistory />} />
+          </>
+        )}
+        {isAdmin && <Route path="/admin" element={<Admin />} />}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Checkout/Shipment.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Shipment = () => {
+  return <div>Shipment</div>;
+};
