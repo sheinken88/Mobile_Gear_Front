ab253dd62cb74d6a8f156a8981529459f2034325
alba-97 <alvarezbasilio97@gmail.com>
2023-05-22 18:50:12 -0300

    se agrego una vista de admin para ver todas las compras

--- src/components/Admin/SalesDashboard.jsx
+++ src/components/Admin/SalesDashboard.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchOrders } from "../../state/orders/ordersActions";
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 
 export const SalesDashboard = () => {
-  return <div>SalesDashboard</div>;
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.orders.orders);
+
+  useEffect(() => {
+    dispatch(fetchOrders());
+    console.log(123, orders);
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Producto</Th>
+            <Th>Cantidad</Th>
+            <Th>Comprador</Th>
+          </Tr>
+        </Thead>
+
+        <Tbody>
+          {orders.map((order) => {
+            return (
+              <Tr>
+                <Td>{order.id}</Td>
+                <Td>{order.product.name}</Td>
+                <Td>{order.qty}</Td>
+                <Td>{order.user.email}</Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </div>
+  );
 };
