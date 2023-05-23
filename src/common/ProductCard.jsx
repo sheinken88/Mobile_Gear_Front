0b251d17ea572fc0cbe4d92be05f1aa75c65d8ea
PC <heinken.sebastian@gmail.com>
2023-05-23 14:35:06 -0300

    agregar al carrito desde el home

--- src/common/ProductCard.jsx
+++ src/common/ProductCard.jsx
@@ -1,10 +1,48 @@
-import { Box, Image, Badge, Flex, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Badge,
+  Flex,
+  Text,
+  VStack,
+  IconButton,
+  useColorModeValue as mode,
+  Tooltip,
+} from "@chakra-ui/react";
+import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { addItemToCart, updateQuantity } from "../state/cart/cartSlice";
 
 export const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+  const cartItems = useSelector((state) => state.cart.items);
+
   const originalPrice = Math.round(
     product.price * (product.discount / 100 + 1)
   );
+
+  const handleAddToCart = (event) => {
+    event.preventDefault();
+    if (!isAuthenticated) alert("Login to add items to your cart");
+    const item = {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      image: product.product_img,
+      quantity: 1,
+    };
+
+    const existingItem = cartItems[item.id];
+
+    if (existingItem) {
+      const newQuantity = existingItem.quantity + item.quantity;
+      dispatch(updateQuantity({ id: item.id, quantity: newQuantity }));
+    } else {
+      dispatch(addItemToCart(item));
+    }
+  };
 
   return (
     <Box
@@ -64,6 +102,25 @@ export const ProductCard = ({ product }) => {
           </Text>
         )}
       </VStack>
+      <Tooltip
+        label="Add to cart"
+        placement="top"
+        hasArrow
+        bg={mode("gray.800", "white")}
+        color={mode("white", "gray.800")}
+      >
+        <IconButton
+          position="relative"
+          top={2}
+          left={2}
+          aria-label="Add to cart"
+          icon={<FaShoppingCart />}
+          size="sm"
+          color={mode("white", "gray.800")}
+          bg={mode("gray.800", "white")}
+          onClick={handleAddToCart}
+        />
+      </Tooltip>
     </Box>
   );
 };
